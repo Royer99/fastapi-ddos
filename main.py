92da63490fae6a8cbc86c4597ffd13426b0c1f68
Royer Donnet Arenas Camacho <A01209400@itesm.mx>
2022-11-08 21:59:51 -0600

    add: query example

--- main.py
+++ main.py
@@ -42,17 +42,19 @@ async def root():
 async def classify(model_parameters: ModelParameters):
 
     absolute_path = os.path.dirname(__file__)
-    if model_parameters.param15 == 1:
+    if model_parameters.model == 1:
         relative_path = "model/model_xgboost99.txt"
-    elif model_parameters.param15 == 2:
+    elif model_parameters.model == 2:
         relative_path = "model/model_xgboost99_semifinal.txt"
-    elif model_parameters.param15 == 3:
+    elif model_parameters.model == 3:
         relative_path = "model/model_xgboost99_semifinal_rate.txt"
 
     full_path = os.path.join(absolute_path, relative_path)
     model = XGBRFClassifier()
     model.load_model(relative_path)
-    test = pd.DataFrame([model_parameters.dict()])
+    params = model_parameters.dict()
+    params.pop('model')
+    test = pd.DataFrame([params])
     test2 = pd.DataFrame(test, columns=['Dur', 'SrcBytes', 'DstBytes', 'TotBytes', 'SrcPkts', 'DstPkts',
                                         'TotPkts', 'SrcRate', 'DstRate', 'Rate', 'Min', 'Max', 'Sum', 'Mean', 'StdDev'], dtype=float)
     prediction = model.predict(test2)
