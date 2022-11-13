3d49257868addd49c38531f30f7ecf8d3aa4a945
Royer Donnet Arenas Camacho <A01209400@itesm.mx>
2022-11-13 14:17:26 -0600

    add: working isolation forest

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from sklearn.preprocessing import StandardScaler
 from xgboost import XGBRFClassifier
 import os
 import pickle
+from sklearn.model_selection import train_test_split
+import joblib
 
 app = FastAPI(title="DDos classifier",
               description="DDos classifier", version="0.1")
@@ -50,27 +52,27 @@ async def classify(model_parameters: ModelParameters):
     elif model_parameters.model == 2:
         relative_path = "model/model_xgboost99_semifinal.txt"
     elif model_parameters.model == 3:
-        relative_path = "model/model_xgboost99_semifinal_rate_noscale.txt"
+        relative_path = "model/myIsolationForest.sav"
 
     full_path = os.path.join(absolute_path, relative_path)
     #model = pickle.load(open(full_path, 'rb'))
     #pickle.load(open(full_path, 'rb'))
-
-    model = XGBRFClassifier()
-    model.load_model(full_path)
+    #model = XGBRFClassifier()
+    # model.load_model(full_path)
+    model = joblib.load(full_path)
     params = model_parameters.dict()
-    # print(params)
     params.pop('model')
 
-    test = pd.DataFrame([params])
+    # # normalize data
+    # scalerpath = "/home/royer/Documents/AD2022/fastapi-ddos/model/std_scaler.sav"
+    # scaler = joblib.load(scalerpath)
+
+    test = pd.DataFrame(params, index=[0])
     test = pd.DataFrame(test, columns=['Dur', 'SrcBytes', 'DstBytes', 'TotBytes', 'SrcPkts', 'DstPkts',
                                        'TotPkts', 'SrcRate', 'DstRate', 'Rate', 'Min', 'Max', 'Sum', 'Mean', 'StdDev'])
     print(test)
     test = test.reindex(columns=['TotPkts', 'TotBytes', 'Dur', 'Mean', 'StdDev', 'Sum', 'Min',
                                  'Max', 'SrcPkts', 'DstPkts', 'SrcBytes', 'DstBytes', 'Rate', 'SrcRate', 'DstRate'])
-    # normalize data
-    # scaler = StandardScaler()
-    # test = scaler.fit_transform(test.T)
     print(test)
     prediction = model.predict(test)
     print(prediction)
